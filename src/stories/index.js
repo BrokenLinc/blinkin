16f48a351f3753ac9ad340b6fe843b5a10cc09b3
Lincoln Anderson <landerson@352inc.com>
2018-11-29 18:42:54 -0500

    Add storybook.

---
+++ src/stories/index.js
@@ -0,0 +1,6 @@
+import React from 'react';
+import { storiesOf } from '@storybook/react';
+
+import { TextInput } from '../lib';
+
+storiesOf('TextInput', module).add('Default', () => <TextInput />);
