ab81aad378e85fbe3173c2816fb267cbc7b89e8a
Lincoln Anderson <landerson@352inc.com>
2018-12-01 11:21:21 -0500

    Restructure spinning ring animation.

--- src/lib/svg/Ring.js
+++ src/lib/svg/Ring.js
@@ -1,30 +1,39 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import styled, { keyframes } from 'styled-components'
+import styled, { css, keyframes } from 'styled-components';
 
 import describeArc from '../util/describeArc';
 import SVG_ATTRIBUTES from '../const/svgAttributes';
 
-const spinnerKeyframes = keyframes`
+const clockwiseKeyframes = keyframes`
   100% {
     transform: rotate(360deg);
   }
 `;
-
-const Spinner = styled.svg`
-  animation: ${spinnerKeyframes} ${({ duration }) => duration}s linear 0s infinite;
+const SpinningPath = styled.path`
+  ${({ duration, origin }) => css`
+    animation: ${clockwiseKeyframes} ${duration}s linear 0s infinite;
+    transform-origin: ${origin[0]}px ${origin[1]}px;
+  `}
 `;
 
-const Ring  = ({ angle, basis, color, duration, size, strokeWidth }) => (
-  <Spinner
+const Ring  = ({ angle, basis, color, duration, size, strokeLinecap, strokeWidth }) => (
+  <svg
     {...SVG_ATTRIBUTES}
-    duration={duration}
     viewBox={[0, 0, basis, basis].join(' ')}
     width={size}
     height={size}
   >
-    <path d={describeArc(basis/2, basis/2, (basis - strokeWidth)/2, 0, angle)} stroke={color} strokeWidth={strokeWidth} fill="none" />
-  </Spinner>
+    <SpinningPath
+      d={describeArc(basis/2, basis/2, (basis - strokeWidth)/2, 0, angle)}
+      duration={duration}
+      fill="none"
+      origin={[basis/2, basis/2]}
+      stroke={color}
+      strokeLinecap={strokeLinecap}
+      strokeWidth={strokeWidth}
+    />
+  </svg>
 );
 Ring.propTypes = {
   angle: PropTypes.number,
@@ -32,6 +41,7 @@ Ring.propTypes = {
   color: PropTypes.string,
   duration: PropTypes.number,
   size: PropTypes.number,
+  strokeLinecap: PropTypes.oneOf(['butt', 'round', 'square']),
   strokeWidth: PropTypes.number,
 };
 Ring.defaultProps = {
