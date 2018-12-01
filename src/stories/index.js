e8024f45b6da85de6d2315255104cc4a38c5ab7d
Lincoln Anderson <landerson@352inc.com>
2018-12-01 11:21:40 -0500

    Add good previews.

--- src/stories/index.js
+++ src/stories/index.js
@@ -2,5 +2,16 @@ import React from 'react';
 import { storiesOf } from '@storybook/react';
 
 import Ring from '../lib/svg/Ring';
+import ShimmerText from '../lib/text/ShimmerText';
 
-storiesOf('Ring', module).add('Default', () => <Ring />);
+storiesOf('Ring', module)
+  .add('Default', () => <Ring />)
+  .add('Color', () => <Ring color="#6523e2" />)
+  .add('Stroke', () => <Ring strokeWidth={24} strokeLinecap="round" />)
+  .add('Angle', () => <Ring angle={120} />)
+  .add('Size', () => <Ring size={40} />)
+  .add('Timing', () => <Ring duration={4} />);
+
+storiesOf('ShimmerText', module)
+  .add('Default', () => <ShimmerText>Hello beautiful world gone awry</ShimmerText>)
+  .add('Duration', () => <ShimmerText duration={5}>Hello beautiful world gone awry</ShimmerText>);
