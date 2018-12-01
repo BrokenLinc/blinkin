3e9d6b380814892257aac7c489224abbc32f2b94
Lincoln Anderson <landerson@352inc.com>
2018-12-01 11:21:31 -0500

    Add ShimmerText.

---
+++ src/lib/text/ShimmerText.js
@@ -0,0 +1,32 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import map from 'lodash/map';
+import times from 'lodash/times';
+import styled, {css, keyframes} from 'styled-components';
+
+const fadeDipKeyFrames = (waves = 1, res = 9, trigFunc = 'cos') => keyframes`
+  ${times(res + 1, (n) => `${n/res * 100}% {
+    opacity: ${Math[trigFunc](n/res * Math.PI * 2 * waves) / 2 + 0.5};
+  }`)}
+`;
+const Letter = styled.span`
+  ${({ duration, interval, waves }) => css`
+    animation: ${fadeDipKeyFrames(waves)} ${duration}s linear ${interval*duration}s infinite;
+  `}
+`;
+
+const ShimmerText = ({ children: text, duration, waves }) => map(text, (letter, index) => (
+  <Letter key={index} duration={duration} interval={(index/text.length) % 1} waves={waves}>{letter}</Letter>
+));
+
+ShimmerText.propTypes = {
+  children: PropTypes.string.isRequired,
+  duration: PropTypes.number,
+  waves: PropTypes.number,
+};
+ShimmerText.defaultProps = {
+  duration: 1,
+  waves: 1,
+};
+
+export default ShimmerText;
