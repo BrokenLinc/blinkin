88e88ed74e9a4b2501a07a533dc29d7214244d6a
Lincoln Anderson <landerson@352inc.com>
2019-02-09 12:50:21 -0500

    Add rint and dToR utilities.

---
+++ src/lib/util/dToR.js
@@ -0,0 +1,5 @@
+const dToR = (degrees) => {
+  return degrees * (Math.PI / 180);
+};
+
+export default dToR;
---
+++ src/lib/util/rint.js
@@ -0,0 +1,5 @@
+const rint = (a, b) => {
+  return ~~((Math.random()*(b-a+1))+a);
+};
+
+export default rint;
